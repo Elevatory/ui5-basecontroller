039d5e334d43ef703fcd2950ffee780df5629b7d
Andre Schüßler <andre@v2202007124979122247.hotsrv.de>
2022-06-04 09:50:15 +0200

    update

--- src/postinstall.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const fsextra = require('fs-extra');
-console.log('Hi, my name is Postinstally 🤓')
-fsextra.copy(process.cwd() + "/node_modules/@gdc/ui5-basecontroller/src/BaseController.ts", process.cwd() + "/src/resources/@gdc/ui5-basecontroller/BaseController.ts", { recursive: true });
-console.log(`I'm done. Hope you are happy.`);
